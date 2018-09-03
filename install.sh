0407502fe93e1b9971a5d0203d3f95f1612fe989
Yogo Yuichi <yogo-yuichi@yogo-yuichi.local>
2018-09-03 15:10:58 +0900

    do brew install after installation

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
+cd ~
+
 # Install Homebrew
 if ! type "brew" > /dev/null; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+mkdir ~/.brewfile
+cp ~/dotfiles/brewfile/Brewfile ~/.brewfile
+
+brew install rcmdnk/file/brew-file
+brew file install
+
 
 # @TODO
 # zplug
