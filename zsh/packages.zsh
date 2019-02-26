c22bae5a18bc194c0a8df7f8c58f4bfa2a2d0a66
Yuichi Yogo <yuichkun@gmail.com>
2019-02-27 00:36:30 +0900

    add gibo

--- zsh/packages.zsh
+++ zsh/packages.zsh
@@ -2,3 +2,4 @@ zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-completions"
 zplug "docker/compose", use:contrib/completion/zsh
+zplug "simonwhitaker/gibo", use:gibo, as:command
