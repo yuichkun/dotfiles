196c64f75fbe199b1a5cf0e1342ceb88e25e2deb
Yuichi Yogo <yuichkun@gmail.com>
2019-02-27 01:41:01 +0900

    add git info to right prompt

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -5,8 +5,19 @@ if [ -f $(brew --prefix)/etc/brew-wrap ];then
   source $(brew --prefix)/etc/brew-wrap
 fi
 
-# Prompt Color
+# Left Prompt
 PROMPT="%F{196}%n %~ %f"
+
+# Right Prompt
+autoload -Uz vcs_info
+setopt prompt_subst
+zstyle ':vcs_info:git:*' check-for-changes true
+zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"
+zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
+zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
+zstyle ':vcs_info:*' actionformats '[%b|%a]'
+precmd () { vcs_info }
+RPROMPT='${vcs_info_msg_0_}'
 
 # ヒストリの設定
 HISTFILE=~/.zsh_history
