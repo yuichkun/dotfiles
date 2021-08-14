7a912605cb668da175e211e1a0fb326eea7c0728
Yuichi Yogo <yuichkun@gmail.com>
2021-08-14 10:32:45 +0900

    clean unused alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,10 +8,8 @@ alias la='ls -a'
 alias ll='ls -l'
 alias c='code .'
 alias o='open .'
-alias t='tmux'
 alias grep='grep  --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}'
 alias git_cleans_what='git clean -fdxn'
-alias yw='yarn workspace'
 
 # Global Aliases
 alias -g G='| grep -i'
