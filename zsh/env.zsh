2778439da603fa0693028f3832113fe24c822ba2
Yuichi Yogo <yuichkun@gmail.com>
2021-12-14 18:25:39 +0900

    rm obsolete commands

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,7 +1,5 @@
 LOG ENV_VARIABLES
 
-export PATH=$HOME/.nodebrew/current/bin:$PATH
 export PATH=$HOME/dotfiles/utils:$PATH
-export GOPATH=$HOME/go
 export LANG=ja_JP.UTF-8
 ENHANCD_FILTER=fzy; export ENHANCD_FILTER
